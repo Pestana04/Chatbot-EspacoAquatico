454a644e2a93fd27dd3146de70c379471ce41f29
Gustavo Pestana <gpestana04@gmail.com>
2026-06-17 22:45:35 -0300

    feat: Dados iniciais

---
+++ banco.py
@@ -0,0 +1,30 @@
+ACADEMIA = {
+    "nome": "Espaço Aquático",
+    "modalidades": [
+        "hidroginástica",
+        "natação infantil",
+        "natação adulto",
+        "hidroterapia",
+        "fisioterapia",
+        "taekwondo"
+    ],
+    "informacoes_pendentes": {
+        "horarios": "Ainda não informado",
+        "valores": "Ainda não informado",
+        "matricula": "Existe matrícula, mas o valor ainda não foi informado",
+        "telefone_atendente": "Ainda não informado"
+    },
+    "contatos": {
+        "telefone": "(32) 3303-0388",
+        "whatsapp": "(32) 3303-0388",
+        "instagram": "@espacoaquaticoacademia",
+        "endereco": "Rua São Mateus, 531 - São Mateus, Juiz de Fora - MG"
+    },
+    "regras_do_bot": [
+        "Pode informar descontos e promoções quando existirem",
+        "Não deve inventar valores ou horários",
+        "Deve encaminhar para atendente quando o cliente pedir atendimento humano",
+        "Deve avisar a atendente quando houver interesse em matrícula ou aula experimental",
+        "Após responder, deve direcionar o cliente para conhecer as atividades da piscina"
+    ]
+}
